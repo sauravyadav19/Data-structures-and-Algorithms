43518c69d320aef2e6491ff40c1559d5c68f3229
Saurav <sauravsaurav0@gmail.com>
2025-09-09 01:40:38 +0000

    Create strcuture for the Node and a class for linked list

--- DataStrctures/linkedList.cpp
+++ DataStrctures/linkedList.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+struct Node{
+    int data;
+    Node* nextPointer;
+    Node(int val) data(val) nextPointer(nullptr) {}
+}
+
+class LinkedList {
+private:
+    Node* head;
+public:
+    LinkedList()head(nullptr){}
+
+};
 int main(){
     return 0;
 }
