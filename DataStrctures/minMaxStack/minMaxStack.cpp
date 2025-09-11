715600b965562a462884236cca9d6166ff2f8a82
Saurav <sauravsaurav0@gmail.com>
2025-09-11 01:29:38 +0000

    Create a skeleton class for implementing stack

--- DataStrctures/minMaxStack/minMaxStack.cpp
+++ DataStrctures/minMaxStack/minMaxStack.cpp
@@ -2,6 +2,25 @@
 #include<vector>
 using namespace std;
 
+class MinMaxStack{
+    private:
+        vector<int> stack = {};
+    public:
+        int peek(){
+            return 0;
+
+        }
+        int pop(){
+            return 0;
+            
+        }
+        int push(){
+
+            return 0;
+        }
+
+};
+
 int main(){
     return 0;
 }
