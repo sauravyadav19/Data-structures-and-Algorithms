ec4490be63cba70eb2787471aed20dba37d7999b
Saurav <sauravsaurav0@gmail.com>
2025-09-11 06:48:57 +0000

    Create the Additional Data Strcture for tracking the min and max values in the stack for a contant access time

--- DataStrctures/minMaxStack/minMaxStack.cpp
+++ DataStrctures/minMaxStack/minMaxStack.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 class MinMaxStack{
     private:
+        vector<unordered_map <string,int>> minMaxStack = {};
         vector<int> stack = {};
     public:
         int peek(){
@@ -12,18 +13,42 @@ class MinMaxStack{
         }
         int pop(){
             int result =  stack[stack.size() - 1];
+            minMaxStack.pop_back();
             stack.pop_back();
             return result;
             
         }
         void push(int value){
-             stack.push_back(value);
+            unordered_map<string,int> newMinMax = {{"min",value},{"max",value}};
+            if(minMaxStack.size()){
+                newMinMax["min"] = min(minMaxStack[minMaxStack.size() -1]["min"], value);
+                newMinMax["max"] = max(minMaxStack[minMaxStack.size() - 1]["max"],value);
+            }
+            minMaxStack.push_back(newMinMax);
+            stack.push_back(value);
         }
         void display(){
             for(auto &element: stack){
                 cout<< element << " ";
             }
             cout<<endl;
+        }
+        void displayMinMax(){
+            for( int i = 0; i < minMaxStack.size(); i++ ){
+                cout<< "Index " << i << ": ";
+                for(auto &element: minMaxStack[i]){
+                    cout<< element.first << ":"<<element.second << " ";
+                }
+                cout<< endl;
+            }
+        }
+        int getMax(){
+            return minMaxStack[minMaxStack.size() - 1]["max"];
+
+        }
+        int getMin(){
+            return minMaxStack[minMaxStack.size() - 1]["min"];
+
         }
 
 };
@@ -37,7 +62,14 @@ int main(){
     obj.pop();
     obj.display();
     obj.push(50);
+    obj.push(2);
+    obj.pop();
+    obj.push(10);
     obj.display();
     cout<< "peek: " << obj.peek() <<endl;
+    cout<< "max value: " << obj.getMax()<<endl;
+    cout<< "min value: " << obj.getMin()<<endl;
+
+    obj.displayMinMax();
     return 0;
 }
