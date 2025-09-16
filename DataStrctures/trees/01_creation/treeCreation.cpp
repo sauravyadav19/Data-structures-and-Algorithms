4ebaad5b1f11ef08fd21a06a274a83582af2e15e
Saurav <sauravsaurav0@gmail.com>
2025-09-16 15:53:43 +0000

    Create a TO-DO list and outline for implenting trees

---
+++ DataStrctures/trees/01_creation/treeCreation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Create a strcutre for represnting Node
+    // it should have an array for storing its children
+
+// create a Tree class
+    // root 
+    // implement functionality to add children for a node
+    // traversal
+
+int main(){
+    return 0;
+}
