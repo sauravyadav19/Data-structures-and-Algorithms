d94237defe51c76159d10356b58e30412870f51e
Saurav <sauravsaurav0@gmail.com>
2025-09-17 08:24:14 +0530

    Create a funtion to display the tree nested to 3 levels

--- DataStrctures/trees/01_creation/treeCreation.cpp
+++ DataStrctures/trees/01_creation/treeCreation.cpp
@@ -12,17 +12,36 @@ class tree{
     private:
         Node* root;
     public:
-        tree(): root(nullptr){}
         tree(Node* node): root(node){}
         void pushChildren(Node* parentNode,Node* child){
             parentNode->children.push_back(child);
         }
+        // Creating a Display function: for showing the tree's level
+        // eg: 10( 20 30 { -101 -102 } 40 { -102 } )
+        // here 10 is the root level elemnet
+        // 20 , 30 40 are root's children
+        // and the elements listed in curly braces after an element is its children
+        //                 10 (root [level 1])
+        //          |      |        |
+        //         20     30        40  [level 2]
+        //               /  \       |
+        //            -101  -102   -103 [level 3]
+
         void display(){
-            cout<< root->data << " --children--> " ;
+            cout<< root->data << "( " ;
             for(int i = 0; i < root->children.size(); i++){
                 cout<< root->children[i]->data <<" ";
+                if(root->children[i]->children.size()){
+                    cout<< "{ ";
+                    for(int j = 0; j < root->children[i]->children.size(); j ++){
+                        cout<< root->children[i]->children[j]->data << " ";
+                    }
+                    cout<< "} ";
+                }
             }
+            cout<< ")" <<endl;;
         }
+       
 
 };
     
@@ -31,10 +50,16 @@ int main(){
     Node* rootLevel1Child1 = new Node(20);
     Node* rootLevel1Child2 = new Node(30);
     Node* rootLevel1Child3 = new Node(40);
+    Node* level2Child1 = new Node(-101);
+    Node* level2Child2 = new Node(-102);
+    Node* level2Child3 = new Node(-103);
    tree t = tree(root);
    t.pushChildren(root,rootLevel1Child1);
    t.pushChildren(root,rootLevel1Child2);
    t.pushChildren(root,rootLevel1Child3);
+   t.pushChildren(rootLevel1Child2,level2Child1);
+   t.pushChildren(rootLevel1Child3,level2Child3);
+   t.pushChildren(rootLevel1Child2,level2Child2);
    t.display();
     return 0;
 }
