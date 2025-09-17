84a8e42a044103f9669001018ff3f92165155952
Saurav <sauravsaurav0@gmail.com>
2025-09-17 07:43:37 +0530

    Create Rudimentary tree-like strcture

--- DataStrctures/trees/01_creation/treeCreation.cpp
+++ DataStrctures/trees/01_creation/treeCreation.cpp
@@ -2,13 +2,39 @@
 #include <vector>
 using namespace std;
 
-// Create a strcutre for represnting Node
-    // it should have an array for storing its children
+struct Node{
+    int data;
+    vector<Node*> children;
+    Node(int data) : data(data), children({}){}
+};
 
-// create a Tree class
-    // root 
-    // implement functionality to add children for a node
+class tree{
+    private:
+        Node* root;
+    public:
+        tree(): root(nullptr){}
+        tree(Node* node): root(node){}
+        void pushChildren(Node* parentNode,Node* child){
+            parentNode->children.push_back(child);
+        }
+        void display(){
+            cout<< root->data << " --children--> " ;
+            for(int i = 0; i < root->children.size(); i++){
+                cout<< root->children[i]->data <<" ";
+            }
+        }
+
+};
     
 int main(){
+    Node* root = new Node(10);
+    Node* rootLevel1Child1 = new Node(20);
+    Node* rootLevel1Child2 = new Node(30);
+    Node* rootLevel1Child3 = new Node(40);
+   tree t = tree(root);
+   t.pushChildren(root,rootLevel1Child1);
+   t.pushChildren(root,rootLevel1Child2);
+   t.pushChildren(root,rootLevel1Child3);
+   t.display();
     return 0;
 }
