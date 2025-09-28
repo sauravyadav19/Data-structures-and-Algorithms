aab693914bcde6f2a90aca8b0d48857f451b80c7
Saurav <sauravsaurav0@gmail.com>
2025-09-28 11:25:15 +0530

    Conceptual overview of the Binary Search Tree and Boilerplate for Implementation

---
+++ DataStrctures/trees/03_BST/01_creation.cpp
@@ -0,0 +1,30 @@
+/*
+    * In arrays, Binary Search cost us O((log n)) but insertion and deletion is costly (shifting of Element)
+    * In Linked List, The insertion and Deletion is easy but the traversal itself cost us O(n)
+
+    * To overcome their individual challanges and get the best of the world, that is 
+        => Searching is easy 
+        => Insertion and Deletion is easy
+    * The new Data strcture that has both of these properties is Binary Search Tree(BST).
+
+    BINARY SEARCH TREE:
+        => As the name suggest, it is a binary tree, meaning, at any node at most it can have 2 children.
+        => but what truly makes BST shines is that, at any given node its
+            => children to the left is always smaller than that of the parent node and
+            => children to the right are always bigger then that of the parent node.
+
+                                    ----------------
+                                    |  parent Node |
+                                    ----------------
+                                    /               \
+                                   /                 \
+                                Smaller             Bigger
+                                
+
+*/
+
+#include <iostream>
+
+int main(){
+    return 0;
+}
