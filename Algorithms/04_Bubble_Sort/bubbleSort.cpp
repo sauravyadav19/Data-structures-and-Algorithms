47f4720e8273b6d5550699f66349f71144c4e311
Saurav <sauravsaurav0@gmail.com>
2025-10-06 22:11:40 +0530

    Program for sorting an array using bubble Sort Algorithm

--- Algorithms/04_Bubble_Sort/bubbleSort.cpp
+++ Algorithms/04_Bubble_Sort/bubbleSort.cpp
@@ -2,6 +2,46 @@
 #include <vector>
 using namespace std;
 
+
+// Helper Function to Swap to values
+void swap(int& num1, int& num2){
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+}
+
+void bubbleSort(vector<int>&array){
+    for (int i = 0; i < array.size() - 1; i++){
+        for (int j = 0; j < array.size()- i - 1; j++){
+            if(array [j] > array[j+1]){
+                swap (array[j], array[j+1]);
+            } 
+        }
+    }
+}
+
+// Helper Function to display array properly
+ostream& operator<< (ostream& stream, vector<int>& array){
+    cout<< "[ ";
+    for (int i = 0; i < array.size(); i++){
+        if(i == array.size() - 1){
+            stream << array[i]<< " ]";
+        }
+        else{
+            stream << array [i] << ", ";
+        }
+    }
+    cout<<endl;
+    return stream;
+}
+
 int main(){
+
+    vector<int> array = {9,10,8,4,2,1,11,15,4};
+    cout<< "Before Sorting : " << array;
+    bubbleSort(array);
+    cout<< "After Sorting :  "<< array;
+
+    
     return 0;
 }
