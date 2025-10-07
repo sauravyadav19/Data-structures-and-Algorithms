4c70b6bf5584d43c610f0e8630c3431b2b610e5d
Saurav <sauravsaurav0@gmail.com>
2025-10-07 13:07:50 +0530

    Program to sort an array using Insertion Sort algorithm

--- Algorithms/04_Sorting_Algorithms/02_Insertion_Sort/insertionSort.cpp
+++ Algorithms/04_Sorting_Algorithms/02_Insertion_Sort/insertionSort.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+ostream& operator<< (ostream&stream, const vector<int>& array);
+
 void insertionSort(vector<int>& array){
+    for (int unsortedIndex = 1; unsortedIndex < array.size(); unsortedIndex++){
+
+        int valueToInsert = array[unsortedIndex];
+        int compareIndex= unsortedIndex;
+
+        // Shifting the sorted array to the right element by element 
+        // until we find the correct position for our 'value-to-be-inserted'
+        while(compareIndex > 0 && array[compareIndex -1] > valueToInsert){
+            array[compareIndex] = array[compareIndex-1];
+            compareIndex--;
+        }
+        // Inserting it to the correct place in the sorted array
+        array[compareIndex] = valueToInsert;
+    }
     return;
 }
 
@@ -22,7 +39,9 @@ ostream& operator<<(ostream& stream, const vector<int>& array){
 
 int main(){
 
-    vector<int> array = {8,5,2,9,5,6,3};
-    cout<< array;
+    vector<int> array = {8,5,2,0,9,-1,0,1,8,2};
+    cout<< "Before Sorting :" << array;
+    insertionSort(array);
+    cout<< "After Sorting :" << array;
     return 0;
 }
