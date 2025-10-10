8d21daa7763f3ee3d1504aa56f7d69a7b0f0ee62
Saurav <sauravsaurav0@gmail.com>
2025-10-10 16:25:52 +0530

    Create the helper function to merge to sorted array

--- Algorithms/04_Sorting_Algorithms/05_Merge_Sort/mergeSort.cpp
+++ Algorithms/04_Sorting_Algorithms/05_Merge_Sort/mergeSort.cpp
@@ -18,10 +18,66 @@ ostream& operator<< (ostream& stream, const vector<int>& array){
     return stream;
 }
 
+vector<int> merge(vector<int>& firstArray, vector<int>& secondArray){
+    vector<int> result = {};
+    if(firstArray.size () != 0 and secondArray.size() != 0){
+       
+        // making the pointer at the begining of the sorted Arrays
+        int firstPointer = 0;
+        int secondPointer = 0;
 
+        while(firstPointer < firstArray.size() and secondPointer < secondArray.size()){
+
+            // appending which ever elemet is the smallest and then moving the pointer to the next element
+            if(firstArray[firstPointer] < secondArray[secondPointer]){
+                result.push_back(firstArray[firstPointer]);
+                firstPointer++ ;
+            }
+            else{
+                result.push_back(secondArray[secondPointer]);
+                secondPointer++;
+                
+            }
+        }
+
+        // in case the length of the array's do not match and one happens to be longer
+        // we simply append all its element to the result vector
+        if(firstPointer >= firstArray.size()){
+
+            while(secondPointer < secondArray.size()){
+                result.push_back(secondArray[secondPointer]);
+                secondPointer++;
+            }
+
+        }
+        else{
+             while(firstPointer < firstArray.size()){
+                result.push_back(firstArray[firstPointer]);
+                firstPointer++;
+            }
+        }
+    }
+    else{
+        // This part make sures that if either or both of the Arrays are empty we 
+        // handle them properly and return the result accordingly
+        if(firstArray.size() == 0 and secondArray.size() == 0){
+            return {};
+        }
+        else if(firstArray.size() == 0){
+            return secondArray;
+        }
+        else{
+            return firstArray;
+        }
+    }
+
+    return result;
+
+}
 
 int main(){
-    vector<int> array = {8,5,2,9,6,7,1};
-    cout<< array;
+    vector<int> array = {8,9,10,15,19};
+    vector<int> array2 = {1,7,10,11,12,20,21};
+   cout<< merge(array,array2);
     return 0;
 }
