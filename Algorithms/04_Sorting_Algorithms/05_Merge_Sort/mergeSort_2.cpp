1be8bdaeda8a4ce86eef0629ed366f71aa9e959b
Saurav <sauravsaurav0@gmail.com>
2025-10-11 15:07:55 +0530

    Create a helper function for merging in a sorted way

--- Algorithms/04_Sorting_Algorithms/05_Merge_Sort/mergeSort_2.cpp
+++ Algorithms/04_Sorting_Algorithms/05_Merge_Sort/mergeSort_2.cpp
@@ -23,6 +23,43 @@ ostream& operator<< (ostream& stream, const vector<int>& array){
     return stream;
 }
 
+void merge(vector<int>& array, unsigned int low, unsigned int mid, unsigned int high){
+
+    vector<int> sorted = {};
+    unsigned int leftPointer = low;
+    unsigned int rightPointer = mid + 1;
+
+    while( leftPointer <= mid && rightPointer <= high){
+        if(array[leftPointer] < array[rightPointer]){
+            sorted.push_back(array[leftPointer]);
+            leftPointer ++;
+        }
+        else{
+            sorted.push_back(array[rightPointer]);
+            rightPointer++;
+        }
+    }
+    if(leftPointer >= mid and rightPointer <= high ){
+        while(rightPointer<= high){
+            sorted.push_back(array[rightPointer]);
+            rightPointer++;
+        }
+    }else{
+        while(leftPointer <= mid){
+            sorted.push_back(array[leftPointer]);
+            leftPointer++;
+        }
+    }
+
+    for(int i = low; i <= high; i++){
+        array[i] = sorted[i];
+    }
+}
+
 int main(){
+    vector<int> array = {3,8,9,7,10,12};
+    cout<< array;
+    merge(array,0,2,5);
+    cout<< array;
     return 0;
 }
