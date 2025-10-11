b6fc89c2f242e48c8cef7b2dec3b9ab593b64c74
Saurav <sauravsaurav0@gmail.com>
2025-10-11 17:14:53 +0530

    state the problem and add the boilerplate

---
+++ Algorithms/05_Recursion/01_Permutation/permutation.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using  namespace std;
+
+// Objective : to find the powerset of an array which has unique element
+// Example 
+    // [1,2,3]
+    //  powerset => [[1,2,3] , [1,3,2], [2,1,3], [2,3,1], [3,1,2], [3,2,1]]
+// Recursion based problem
+
+vector<vector<int>> getAllPermutation(vector<int>& array){
+    return {};
+}
+
+int main(){
+
+
+
+    return 0;
+}
