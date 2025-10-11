0b794c903c6efbf9450b29ffc925901f42eb626a
Saurav <sauravsaurav0@gmail.com>
2025-10-11 15:29:45 +0530

    Program to sort the array using merge sort

--- Algorithms/04_Sorting_Algorithms/05_Merge_Sort/mergeSort_2.cpp
+++ Algorithms/04_Sorting_Algorithms/05_Merge_Sort/mergeSort_2.cpp
@@ -26,9 +26,11 @@ ostream& operator<< (ostream& stream, const vector<int>& array){
 void merge(vector<int>& array, unsigned int low, unsigned int mid, unsigned int high){
 
     vector<int> sorted = {};
+
+    // Pointers to both sides of "array"
     unsigned int leftPointer = low;
     unsigned int rightPointer = mid + 1;
-
+    // Comparing the elements 
     while( leftPointer <= mid && rightPointer <= high){
         if(array[leftPointer] < array[rightPointer]){
             sorted.push_back(array[leftPointer]);
@@ -39,27 +41,48 @@ void merge(vector<int>& array, unsigned int low, unsigned int mid, unsigned int 
             rightPointer++;
         }
     }
-    if(leftPointer >= mid and rightPointer <= high ){
-        while(rightPointer<= high){
-            sorted.push_back(array[rightPointer]);
-            rightPointer++;
-        }
-    }else{
-        while(leftPointer <= mid){
-            sorted.push_back(array[leftPointer]);
-            leftPointer++;
-        }
+
+    // copying the leftovers
+    while(rightPointer<= high){
+        sorted.push_back(array[rightPointer]);
+        rightPointer++;
+    }
+    while(leftPointer <= mid){
+        sorted.push_back(array[leftPointer]);
+        leftPointer++;
     }
 
-    for(int i = low; i <= high; i++){
-        array[i] = sorted[i];
+    // copying the value from temporary array to our actual array
+    for(int i = 0; i < sorted.size(); i++){
+        // our temp array starts from zero but it will happen that 
+        // our actual array does not so finding the correct position using low +i
+        array[low+i] = sorted[i];
     }
 }
 
+void mergeSortHelper(vector<int>& array,int left, int right){
+
+    // Base case in case the pointer crossed
+    if(left >= right ){
+        return;
+    }
+    // finding the mid value
+    int mid = (left + right) / 2;
+    // recursively dividing the array
+    mergeSortHelper(array,left,mid);
+    mergeSortHelper(array,mid + 1,right);
+    // merging the sorted parts
+    merge(array,left,mid,right);
+
+}
+
+void mergeSort(vector<int>& array){
+    mergeSortHelper(array,0,array.size() - 1);
+}
 int main(){
-    vector<int> array = {3,8,9,7,10,12};
-    cout<< array;
-    merge(array,0,2,5);
-    cout<< array;
+    vector<int> array = {9,-1,0,4,7,11,3,6,7};
+    cout<< "Before Sorting : " << array;
+    mergeSort(array);
+     cout<< "After Sorting : " << array;
     return 0;
 }
