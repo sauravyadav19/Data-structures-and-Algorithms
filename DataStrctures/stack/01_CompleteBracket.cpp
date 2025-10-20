995a050b06e8709aa38afd8e1dcfdb67fd0fc056
Saurav <sauravsaurav0@gmail.com>
2025-10-20 15:15:03 +0530

    Add a helper function to display a stack in a formatted manner

--- DataStrctures/stack/01_CompleteBracket.cpp
+++ DataStrctures/stack/01_CompleteBracket.cpp
@@ -1,8 +1,19 @@
 #include<iostream>
 #include<vector>
 #include <string>
+#include <stack>
 using namespace std;
 
+ostream& operator<<(ostream& stream, stack<char>& string ){
+    stream<<endl;
+
+    while(!string.empty()){
+        stream << "|\t"<<string.top() << "\t|"<<endl;
+        stream <<"|_______________|"<<endl;
+        string.pop();
+    }
+    return stream;
+}
  
 int main(){
 
