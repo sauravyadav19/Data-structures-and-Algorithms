5ade97fe2ff069b74c7d811c9aeea32708f2b092
Saurav <sauravsaurav0@gmail.com>
2025-10-21 07:38:03 +0530

    Approach to solve the sunset view problem

--- DataStrctures/stack/02_sunsetViews.cpp
+++ DataStrctures/stack/02_sunsetViews.cpp
@@ -49,7 +49,24 @@ ostream& operator<<(ostream& stream, stack<int> string ){
     return stream;
 }
 
+// Approach To solve this 
+/*
+    We will use stack to solve this problem,
+        - Keep pushing elements(height of the buildings) into the stack
+        - until a bigger element (building with greater height) than the last value in the stack appears or 
+          we ran out of elements(complete the traversal)
+            -  if the current building's height is greater than the top most element in the stack
+            -  we keep poping the elements(building) from the stack until either we find a elment that 
+                bigger or the stack is empty;
 
+            - this way after the traversal of the entire array we would be left with only the indices that
+             - a visible sunset from them.
+             
+             - we pop them and store the result in an array, 
+
+             ----if the direction is WEST (sun is on the left of zeroth index we would start the stackifiation of the
+                    array in the reverse order that is from n to zero instead of what we did for EAST (zeroth to n));
+*/
 int main(){
     return 0;
 }
