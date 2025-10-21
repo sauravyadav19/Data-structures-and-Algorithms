b646e507172c196df8cc578b7178db8e1873e2e3
Saurav <sauravsaurav0@gmail.com>
2025-10-21 08:10:40 +0530

    Write a program to find the index of the buildings from where the sunset would be visible

--- DataStrctures/stack/02_sunsetViews.cpp
+++ DataStrctures/stack/02_sunsetViews.cpp
@@ -34,6 +34,7 @@ IN CASE THE Direction is WEST (sun is at the left of the zeroth Index)
 
 #include <iostream>
 #include <vector>
+#include <stack>
 using namespace std;
 
 
@@ -67,6 +68,58 @@ ostream& operator<<(ostream& stream, stack<int> string ){
              ----if the direction is WEST (sun is on the left of zeroth index we would start the stackifiation of the
                     array in the reverse order that is from n to zero instead of what we did for EAST (zeroth to n));
 */
+
+vector<int> sunsetView(vector<int>& buildingsHeight, string direction){
+    stack<int> buildingsWithView;
+
+    // Traversing the array
+    for(int i = 0; i < buildingsHeight.size(); i++){
+        // In case the stack is empty, we simply push the building's index to stack
+        if(buildingsWithView.empty()){
+            buildingsWithView.push(i);
+        }else{
+            // If the building at the top of the stack has greater height than the current building
+            // we simply push the current builidng's index to the stack
+            if (buildingsHeight[buildingsWithView.top()] > buildingsHeight[i]){
+                buildingsWithView.push(i);
+            }
+            else{
+                // and in case the top building's height is smaller than that of current's,
+                // we simply keep removing the building's index  until either stack is empty
+                // or we encouter a building that has greater height that our current building
+                // at the end of it we simply push the index of our current building's index to the stack
+                while(!buildingsWithView.empty() and buildingsHeight[buildingsWithView.top()] <= buildingsHeight[i]){
+                    buildingsWithView.pop();
+                }
+                buildingsWithView.push(i);
+            }
+        }
+    }
+
+    // this is our final array with index of the buildings from where the sunset will be visible
+
+    vector<int> result;
+    while(!buildingsWithView.empty()){
+        int top = buildingsWithView.top();
+        // we are taking the top element and inserting it in the front as oppose to back, as we need
+        // a sorted array;
+        result.insert(result.begin(), top);
+        buildingsWithView.pop();
+        
+    }
+    // returing the result
+    return result;
+
+
+}
+
 int main(){
+
+    vector<int> buildingsHeight = {3,5,4,4,3,1,3,2};
+    vector<int> result = sunsetView(buildingsHeight,"EAST");
+    for(auto el: result){
+        cout<< el << " ";
+    }
+    cout<<endl;
     return 0;
 }
