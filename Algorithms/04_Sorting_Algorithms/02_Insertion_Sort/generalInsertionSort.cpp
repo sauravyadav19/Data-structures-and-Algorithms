f9aafb52447add60741f1114473a9f5aad494742
Saurav <sauravsaurav0@gmail.com>
2025-12-19 13:10:27 +0530

    Using template to implement insertion sort

--- Algorithms/04_Sorting_Algorithms/02_Insertion_Sort/generalInsertionSort.cpp
+++ Algorithms/04_Sorting_Algorithms/02_Insertion_Sort/generalInsertionSort.cpp
@@ -16,7 +16,28 @@ ostream& operator<<(ostream& stream, const vector<A>& array){
   return stream;
 }
 
+template <typename T>
+void insertionSort(vector<T> &array){
+  for(unsigned int unsortedIndex = 1; unsortedIndex < array.size(); unsortedIndex ++){
+   T valueToInsert = array[unsortedIndex];
+   int compareIndex = unsortedIndex;
+   while(array[compareIndex - 1] > valueToInsert && compareIndex > 0){
+    array[compareIndex] = array[compareIndex - 1];
+    compareIndex --;
+   }
+   array[compareIndex] =  valueToInsert;
+  } 
+  
+  return;
+}
+
 int main(){
 
+  // vector<char> array = {'c', 'm' , 'z', 'b', 'A', 'k'};
+  vector<int> array = {9,3,7,1,10,5};
+  // vector<float> array = {1.234, 2.24,2.22,0.8,-0.89,0.21,9.99,1.2};
+  cout<< array<<endl;
+  insertionSort(array);
+  cout<< array<<endl;
   return 0;
 }
