983225f62e33aa02c3f6983b03cc3c087a4a99ce
Saurav <sauravsaurav0@gmail.com>
2025-12-22 20:51:33 +0530

    Implementing merge sort using ping-pong method: Creating boilerplate

---
+++ Algorithms/04_Sorting_Algorithms/05_Merge_Sort/ping-pongMergeSort.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+template < typename A>
+ostream& operator<<(ostream& stream, const vector<A>& array){
+  stream << "[ ";
+  for(int i = 0; i < array.size(); i++){
+    if(i == array.size() - 1){
+      stream << array[i];
+    }else{
+      stream << array [i]<< ", ";
+  } 
+  }
+  stream << " ]"; 
+  return stream;
+}
+
+
+int main(){
+    return 0;
+}
