25b7263a4a2708ddac71951456fdc1f888273636
Saurav <sauravsaurav0@gmail.com>
2025-12-23 12:06:02 +0530

    ping-pong merge sort: Adding function to merge two sorted array

--- Algorithms/04_Sorting_Algorithms/05_Merge_Sort/ping-pongMergeSort.cpp
+++ Algorithms/04_Sorting_Algorithms/05_Merge_Sort/ping-pongMergeSort.cpp
@@ -16,6 +16,38 @@ ostream& operator<<(ostream& stream, const vector<A>& array){
   return stream;
 }
 
+void merge(vector<int>& array, vector<int>& auxillaryArray, int low, int mid, int high ){
+
+     int pt1= low;
+     int pt2 = mid;
+     int auxI = low;
+     
+     
+     while(pt1 < mid && pt2 < high){
+        if(array[pt1] <=  array[pt2]){
+            auxillaryArray[auxI] = array[pt1];
+            pt1++;
+        }
+        else{
+            auxillaryArray[auxI] = array[pt2];
+            pt2++;
+        }
+        auxI++;
+     }
+    while(pt1< mid){
+        auxillaryArray[auxI] = array[pt1];
+        auxI++;
+        pt1++;
+    }
+
+    while(pt2 < high){
+        auxillaryArray[auxI] = array[pt2];
+        auxI++;
+        pt2++;
+    }
+ 
+}
+
 
 int main(){
     return 0;
