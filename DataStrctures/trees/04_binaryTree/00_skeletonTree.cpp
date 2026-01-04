4403b724732d402c6a3ce547462c775187eea4da
Saurav <sauravsaurav0@gmail.com>
2026-01-04 19:41:00 +0530

    Add comments for references,code for depth first and breadth first orders

--- DataStrctures/trees/04_binaryTree/00_skeletonTree.cpp
+++ DataStrctures/trees/04_binaryTree/00_skeletonTree.cpp
@@ -1,14 +1,35 @@
+/*
+    This is a skeleton program that is meant to assist someone in creating a binary tree , all they have
+    to do is give the level order traversal in a list-like form;
+    {1,2,3,4,5,6,7,8,9}  will create a tree like following
+
+                                    1                           => Level 1
+                               /        \
+                             /           \
+                            /             \
+                            2             3                     => Level 2
+                          /   \         /   \
+                         /     \       /     \
+                        4      5      6      7                  => Level 3
+                      /   \
+                     /     \
+                    8      9                                    => Level 4
+*/  
+
+
 #include<iostream>
 #include<queue>
 #include<optional>
 using namespace std;
+
+// Template is used to  make this program generic, so we can create binary tree of any datatype alike.
 
 template <typename T>
 struct Node{
     T value;
     Node* left;
     Node* right;
-    Node(T value):value(value), left(nullptr), right(nullptr){}
+    Node(const T &value):value(value), left(nullptr), right(nullptr){}
 };
 template <typename T>
 class BinaryTree{
@@ -16,20 +37,73 @@ class BinaryTree{
         Node<T>* root;
     public:
         BinaryTree(Node<T>* rootNode):root(rootNode){}
+        /* what and why data strcture used here are for
+            1. Initializer List: we want to give the user a simple user interface so they do not have
+                care strcutring of the data; all they have to do is, specify data type of the binary tre
+                they want to create the name of tree and the nodes in a list like form:
+                    binaryTree<int> myTree = {1,2,3,4,5,6,7};
+            2. optional: there are times that when we want to create a tree like this: 
+
+                                    1                           => Level 1
+                               /        \
+                             /           \
+                            /             \
+                          null            3                     => Level 2
+                                        /   \
+                                       /     \
+                                      6       7                  => Level 3
+                
+                as we can see that we want to left subtree of the root to be pointing to nothing (nullptr)
+                and this will create a list like this {1,nullptr,3,6,7}, there is no way to store null to 
+                a int type or absence of a value; 
+                    int x = 5; (valid)
+                    int x = null  (X not allowed)
+                so to represnt a absence of value, C++ came up with optional<T>
+                    optional<int> x = 5;  (allowed)
+                    optional<int> x = nullopt (absence of a value is also allowed)
+
+                x.has_value() tell whether the container has a value or not and if it has a value 
+                we can access it using x.value();
+                so in our program we were able to create a initializer list of optional containers for a 
+                generic type so it can also hold null values something like this {1,nullopt,3,4,5,6};
+            3. Queue: We are taking values from the intialzier list and then creating a node, but we need
+                a systemic way to add children nodes to parents, queue help us to do exactly that by manintaing
+                a the order in which the nodes should be inserted and on which side of the parent node
+                
+
+        */
         BinaryTree(initializer_list<optional<T>> list){   
             if(list.size() == 0 or !list.begin()->has_value()){
                 this->root = nullptr;
                 return;
             }                         
            
+            // Taking first value of the list as the root node
             this->root = new Node<T> (list.begin()->value());  
             queue<Node<T>*> insertingOrder;
             insertingOrder.push(root);
+            // an iterator to traverse the intializer list 
             const optional<T>* listIterator = list.begin() + 1;
 
             while(listIterator != list.end() && !insertingOrder.empty()){
+                // Taking the front most value from the Queue as sotring it as node to do the processing
+                // Poping the front most value from the Queue
+                // taking the value from the list and adding it to the left side of the current parent node, if the container there has a value
+                // and then adding this newly added child node to end of the queue
+                // incrementing the list iterator
+               // taking the value from the list and adding it to the right side of the current parent node, if the container there has a value
+                // and then adding this newly added child node to end of the queue
+                // incrementing the list iterator
+                // all this is done untill either the list is empty or the queue is empty;
+
+                // side note: it is understandable why we are checking whether the list is empty or not
+                // but what about the queue, why are we checking that whether that is empty or not, to answer that
+                // we need  a case, suppose we have list like this {1,nullopt,nullopt,2,3}; here even though the list has
+                // more values, the queue is empty and the leftover values cannot be added, thus we need to handle this case
+                // gracefully and exit without throwing error.
                 Node<T>* currentParentNode = insertingOrder.front();
                 insertingOrder.pop();
+                // Left side
                 if(listIterator != list.end()){
                     if(listIterator->has_value()){
                         currentParentNode->left = new Node<T>(listIterator->value());
@@ -37,6 +111,8 @@ class BinaryTree{
                     }
                     listIterator++;
                 }
+
+                // Right side
 
                 if(listIterator != list.end()){
                     if(listIterator->has_value()){
@@ -52,11 +128,15 @@ class BinaryTree{
        const  Node<T>* getRoot(){
             return this->root;
         }
+        void printTreeHelper(const Node<T>*, int space = 0, int indent = 5);
+        void depthFirstOrder(const Node<T>*, vector<T>&);
+        void breadthFirstOrder(const Node<T>* ,vector<T>&);
 
 };
 
+// a method to print the tree for debugging purposes
 template <typename T>
-  void printTreeHelper(const Node<T>* root, int space = 0, int indent = 5) {
+void BinaryTree<T>::printTreeHelper(const Node<T>* root, int space, int indent) {
            
             if (root == nullptr){
                 return;
@@ -80,24 +160,63 @@ template <typename T>
         }
 
 
-
+// a method of the Binary tree class to return the depth first order of the nodes.
 template <typename T>
-void dfs(const Node<T>* node){
-
+void BinaryTree<T>::depthFirstOrder (const Node<T>* node, vector<T>& depthFirstOrderArray){
     if(node == nullptr){
         return;
     }
-    cout<< node->value << " ";
-    dfs(node->left);
-    dfs(node->right);
+    depthFirstOrderArray.push_back(node->value);
+    depthFirstOrder(node->left,depthFirstOrderArray);
+    depthFirstOrder(node->right,depthFirstOrderArray); 
+}
+
+
+// a method of the binary tree class to return the breadth first order of the nodes.
+template <typename T>
+void BinaryTree<T>::breadthFirstOrder(const Node<T>* node, vector<T>& breadthFirstOrderArray){
+    if(node == nullptr){
+        return;
+    }
+    queue<const Node<T>*> order;
+    order.push(node);
+
+    while(!order.empty()){
+        const Node<T>* currentNode = order.front();
+        breadthFirstOrderArray.push_back(currentNode->value);
+        if(currentNode->left != nullptr){
+            order.push(currentNode->left);
+        }
+        if(currentNode->right != nullptr){ 
+            order.push(currentNode->right);
+        }
+        order.pop();
+    }
     
 }
 
 int main(){
-    BinaryTree<int> b = {1,2,3,4,5,6,7,8,9,10};
+   BinaryTree<int> b = {1,2,5,3,4,6,7,8,nullopt,nullopt,11,nullopt,nullopt,nullopt,9};
    const Node<int>* root = b.getRoot();
-   printTreeHelper(root);
-   cout<< root<<endl;
-   dfs(root);
+
+   b.printTreeHelper(root);
+   
+   vector<int> df;
+   b.depthFirstOrder(root,df);
+
+   vector<int> bf;
+   b.breadthFirstOrder(root,bf);
+
+   cout<< "Depth first order: ";
+   for(int&i: df){
+    cout<< i << " ";
+   }
+   cout<<endl;
+
+   cout<<"Breadth first order: ";
+   for(int& i : bf){
+    cout<< i << " ";
+   }
+   cout<<endl;
     return 0;
 }
