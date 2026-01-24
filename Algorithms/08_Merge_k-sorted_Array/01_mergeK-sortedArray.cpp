3124ea3a96a54a68513c684b7de0c272fe89d7df
Saurav <sauravsaurav0@gmail.com>
2026-01-24 21:34:46 +0530

    Using minHeap implement the program to merge k-sorted arrays

--- Algorithms/08_Merge_k-sorted_Array/01_mergeK-sortedArray.cpp
+++ Algorithms/08_Merge_k-sorted_Array/01_mergeK-sortedArray.cpp
@@ -56,6 +56,13 @@ class MinHeap{
             }
             heapify();
         }
+        MinHeap(vector<T> list){
+            heap.reserve(list.size());
+            for(auto iterator = list.begin(); iterator < list.end(); iterator++){
+                heap.push_back(*iterator);
+            }
+            heapify();
+        }
         
        
         void insert(T newValue){
@@ -77,7 +84,7 @@ class MinHeap{
         }
 
 
-        T peek() const{
+       const T& peek() const{
             if(isEmpty()){
                 throw runtime_error("Peeking Failed: Heap is Empty");
             }else{
@@ -100,8 +107,80 @@ class MinHeap{
         }
 };
 
+template <typename T>
+struct item{
+    T element;
+    size_t index;
+    size_t arrayPart;
+
+    bool operator<(const item<T>& other)const{
+        return element < other.element;
+    }
+
+    bool operator>(const item<T>& other) const {
+        return element > other.element;
+    }
+    item& operator=(const item<T>& other){
+        element = other.element;
+        index = other.index;
+        arrayPart = other.arrayPart;
+        return *this;
+    }
+};
+
+template <typename T>
+ ostream& operator<<(ostream& stream,const item<T> it) {
+        stream<< "{ " << it.element << " at "<< it.index << " of " << it.arrayPart << " }";
+        return stream;
+    }
+
+
+
+template <typename T>
+vector<T> minHeapManager(vector<vector<T>>& arrays){
+
+    vector<T> finalSorted;
+    vector<item<T>> smallestItems;
+    for(size_t i = 0; i < arrays.size();i++){
+        smallestItems.push_back({
+            arrays[i][0],
+            0,
+            i
+        } );
+    }
+    MinHeap<item<T>> heap = smallestItems;
+
+    while(!heap.isEmpty()){
+        item<T> PreviousTopMostElement = heap.peek();
+        finalSorted.push_back(heap.removeRoot().element);
+        if(PreviousTopMostElement.index + 1 < arrays[PreviousTopMostElement.arrayPart].size()){
+            item<T> nextToBeInserted = {    
+                arrays[PreviousTopMostElement.arrayPart][PreviousTopMostElement.index+1],
+                PreviousTopMostElement.index+1,
+                PreviousTopMostElement.arrayPart
+            };
+            heap.insert(nextToBeInserted);
+        }
+        heap.printHeap();
+    }
+
+    return finalSorted;
+
+
+}
+
+   
+
 
 int main(){
 
+    vector<vector<int>> arrays = {{1,5,9,21},{-1,0},{-124,81,121},{3,6,12,20,156}};
+   
+    vector<int> finalSorted = minHeapManager(arrays);
+    cout<<endl;
+    for(int & i : finalSorted){
+        cout<< i<< " ";
+    }
+    cout<<endl;
     return 0;
 }
