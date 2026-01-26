90b2339fb10776d04b013267bacbd640ef631807
Saurav <sauravsaurav0@gmail.com>
2026-01-26 17:05:44 +0530

    Create a generic Heap strcture with a generic compare capability

---
+++ Algorithms/09_Laptop_Rentals/01_LaptopRentalV1.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <vector>
+#include <initializer_list>
+#include <algorithm>
+using namespace std;
+
+template <typename T, typename Compare = std::less<T>>
+class Heap{
+    private:
+        vector<T> heap;
+        Compare compare;
+
+         void siftUp(size_t index){
+            size_t parentNode = (index - 1)/2;
+            while(index > 0 && compare(heap[index],heap[parentNode])){
+                swap(heap[parentNode],heap[index]);
+                index = parentNode;
+                parentNode = (index - 1)/2;
+            }
+        } 
+
+
+         void siftDown(size_t index){
+            size_t leftChild = (2*index + 1);
+            size_t rightChild = (2*index + 2);
+            if(leftChild >= heap.size()){
+                return;
+            }
+            size_t swapWith = rightChild < heap.size() && compare(heap[rightChild],heap[leftChild])
+                            ? rightChild
+                            : leftChild;
+            while(leftChild < heap.size() &&  compare(heap[swapWith],heap[index])){
+                    swap(heap[index], heap[swapWith]);
+                    index = swapWith;
+                    leftChild = (2*index + 1);
+                    rightChild = (2*index + 2);
+                    if(leftChild >= heap.size()) break;
+                    swapWith = (rightChild < heap.size() && compare(heap[rightChild],heap[leftChild]))
+                                ? rightChild
+                                : leftChild;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+            } 
+
+        }
+
+
+        void heapify(){
+            for (int i = (int)heap.size() / 2 - 1; i >= 0; --i) {
+                siftDown(i);
+            }
+        }
+      
+   
+    public:
+        
+        Heap(){}
+        
+        Heap(initializer_list<T> list){
+            heap.reserve(list.size());
+            for(auto iterator = list.begin(); iterator < list.end(); iterator++){
+                heap.push_back(*iterator);
+            }
+            heapify();
+        }
+
+
+        explicit Heap(vector<T>&& list) : heap(std::move(list)) {
+            heapify();
+        }
+        
+       
+        void insert(T newValue){
+            heap.push_back(newValue);
+            siftUp(heap.size() - 1);
+        }
+
+
+        T removeRoot(){
+            if(!isEmpty()){
+                swap(heap[0], heap[heap.size() - 1]);
+                T removedElement = heap[heap.size() - 1];
+                heap.pop_back();
+                if(!isEmpty()){
+                    siftDown(0);
+                }
+                return removedElement;
+            }else{
+                throw runtime_error("Removal Failed: Heap is Empty");
+            }
+        }
+
+
+       const T& peek() const{
+            if(isEmpty()){
+                throw runtime_error("Peeking Failed: Heap is Empty");
+            }else{
+                return heap[0];
+            }
+        }
+
+
+        bool isEmpty () const{
+            return heap.empty();
+        }
+
+
+       void print() const{
+            cout << "{ ";
+            for (const T& i : heap) {
+                cout << i << " ";
+            }
+            cout << "}"<<endl;
+        }
+
+
+        size_t size() const {
+            return heap.size();
+        }
+
+};
+
+
+ostream& operator<<(ostream& stream, vector<int> vector){
+    stream<< "{ ";
+    for(int i = 0; i < vector.size(); i++){
+        stream<< vector[i] << " ";
+    }
+    stream<< "}";
+
+    return stream;
+    
+}
+
+int main(){
+
+    Heap<int> minheap = {9,1,3,5,6,8,3,7,2,8,53,9};
+    minheap.print();
+    Heap<int, greater<int>> maxHeap = {9,1,3,5,6,8,3,7,2,8,53,9};
+    maxHeap.print();
+    return 0;
+
+}
